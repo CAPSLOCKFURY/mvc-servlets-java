a61fbefcf1651d60e8f068fda96815dbec59496f
vadim.dembitskii@gmail.com <vadim.dembitskii@gmail.com>
2022-03-02 12:18:33 +0200

    made ClassUtils final

--- src/main/java/utils/ClassUtils.java
+++ src/main/java/utils/ClassUtils.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-public class ClassUtils {
+public final class ClassUtils {
 
     public static List<Class<?>> getClassesInPackage(String packageName){
         //TODO add recursive directories support
