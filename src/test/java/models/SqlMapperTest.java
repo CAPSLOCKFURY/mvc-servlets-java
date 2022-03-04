37d0180d9a35fb3bfe3ca5d858f6bd7905d76bb0
vadim.dembitskii@gmail.com <vadim.dembitskii@gmail.com>
2022-03-04 18:38:22 +0200

    Refactored code for SqlMapperTest

--- src/test/java/models/SqlMapperTest.java
+++ src/test/java/models/SqlMapperTest.java
@@ -4,6 +4,7 @@ import forms.resourses.StringIntPair;
 import forms.resourses.StringPair;
 import models.base.SqlMapper;
 import models.resources.TestModel;
+import models.resources.TypePair;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -11,6 +12,7 @@ import org.mockito.Mockito;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -19,13 +21,15 @@ public class SqlMapperTest {
 
     @ParameterizedTest
     @MethodSource("sqlMapperCases")
-    public void testMapper(StringPair[] pairs, StringIntPair[] stringIntPairs, Object model, String expected) throws SQLException {
+    public void testMapper(TypePair[] typePairs, Object model, String expected) throws SQLException {
         ResultSet rs = Mockito.mock(ResultSet.class);
-        for(StringPair pair : pairs){
-            Mockito.when(rs.getString(pair.getKey())).thenReturn(pair.getValue());
-        }
-        for (StringIntPair pair : stringIntPairs){
-            Mockito.when(rs.getInt(pair.getKey())).thenReturn(pair.getValue());
+        for(TypePair pair : typePairs){
+            if(pair.getType() == String.class){
+                Mockito.when(rs.getString((String)pair.getKey())).thenReturn((String)pair.getValue());
+            }
+            if(pair.getType() == Integer.class){
+                Mockito.when(rs.getInt((String)pair.getKey())).thenReturn((Integer) pair.getValue());
+            }
         }
         SqlMapper modelMapper = new SqlMapper(model);
         modelMapper.mapFromResultSet(rs);
@@ -35,8 +39,13 @@ public class SqlMapperTest {
     public static Stream<Arguments> sqlMapperCases(){
         return Stream.of(
                 Arguments.of(
-                        new StringPair[]{new StringPair("field", "fieldValue"), new StringPair("stringField", "stringFieldValue")},
-                        new StringIntPair[]{new StringIntPair("intField", 1)},
+                        //new StringPair[]{new StringPair("field", "fieldValue"), new StringPair("stringField", "stringFieldValue")},
+                        //new StringIntPair[]{new StringIntPair("intField", 1)},
+                        new TypePair[]{
+                                new TypePair<String, String, String>("field", "fieldValue", String.class),
+                                new TypePair<String, Integer, Integer>("intField", 1, Integer.class),
+                                new TypePair<String, String, String>("stringField", "stringFieldValue", String.class)
+                        },
                         new TestModel(),
                         "TestModel{" +
                                 "field='" + "fieldValue" + '\'' +
--- src/test/java/models/resources/TypePair.java
+++ src/test/java/models/resources/TypePair.java
@@ -4,14 +4,14 @@ import forms.resourses.Pair;
 
 public class TypePair<K, V, T> extends Pair<K, V> {
 
-    private final T type;
+    private final Class<T> type;
 
-    public TypePair(K k, V v, T t) {
+    public TypePair(K k, V v, Class<T> t) {
         super(k, v);
         type = t;
     }
 
-    public T getType(){
+    public Class<T> getType(){
         return type;
     }
 }
