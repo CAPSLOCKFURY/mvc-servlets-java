b6d2c0748b866b22c34d4c1594298f0c92b61978
vadim.dembitskii@gmail.com <vadim.dembitskii@gmail.com>
2022-03-14 17:20:08 +0200

    added RoomsServiceTest

---
+++ src/test/java/service/RoomsServiceTest.java
@@ -0,0 +1,98 @@
+package service;
+
+import db.ConnectionPool;
+import forms.BookRoomForm;
+import models.Room;
+import models.RoomClass;
+import models.base.ordering.OrderDirection;
+import models.base.ordering.Orderable;
+import models.base.pagination.Pageable;
+import models.dto.RoomExtendedInfo;
+import models.dto.RoomHistoryDTO;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class RoomsServiceTest {
+
+    private final static RoomsService service = RoomsService.getInstance();
+
+    @BeforeAll
+    public static void setUp(){
+        ConnectionPool.initPool();
+    }
+
+    @Test
+    public void getAllRoomsTest(){
+        Orderable orderable = new Orderable("id", OrderDirection.ASC);
+        Pageable pageable = new Pageable(1, 10);
+        List<Room> rooms = service.getAllRooms("en", orderable, pageable);
+        assertEquals(10, rooms.size());
+        assertEquals("Cheap", rooms.get(0).getClassName());
+    }
+
+    @Test
+    public void getAllRoomClassesTest(){
+        List<RoomClass> roomClasses = service.getRoomClasses("en");
+        assertEquals(4, roomClasses.size());
+    }
+
+    @Test
+    public void getRoomById(){
+        Room room = service.getRoomById(1L, "en");
+        assertEquals(1, room.getId());
+        assertEquals("Cheap", room.getClassName());
+    }
+
+    @Test
+    public void getExtendedRoomInfoTest(){
+        RoomExtendedInfo roomExtendedInfo = service.getExtendedRoomInfo(1L, "en");
+        assertEquals(1, roomExtendedInfo.getId());
+        assertEquals(1, roomExtendedInfo.getDates().size());
+        LocalDateTime today = new java.sql.Date(System.currentTimeMillis()).toLocalDate().atStartOfDay();
+        assertEquals(today, roomExtendedInfo.getDates().get(0).getCheckInDate().toLocalDate().atStartOfDay());
+        LocalDateTime todayPlus7 = today.plusDays(7);
+        assertEquals(todayPlus7, roomExtendedInfo.getDates().get(0).getCheckOutDate().toLocalDate().atStartOfDay());
+    }
+
+    @Test
+    public void getUserRoomHistoryTest(){
+        Pageable pageable = new Pageable(1, 10);
+        List<RoomHistoryDTO> roomHistory = service.getUserRoomHistory(1L, "en", pageable);
+        assertEquals(1, roomHistory.size());
+        assertEquals(1, roomHistory.get(0).getId());
+    }
+
+    @Test
+    public void bookRoomTest(){
+        BookRoomForm form = new BookRoomForm();
+
+        LocalDateTime today = new java.sql.Date(System.currentTimeMillis()).toLocalDate().atStartOfDay();
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String stringToday = today.format(dateFormat);
+
+        form.setCheckInDate(stringToday);
+
+        LocalDateTime todayPlus7 = today.plusDays(7);
+        form.setCheckOutDate(todayPlus7.format(dateFormat));
+        form.setLocale(new Locale("en"));
+
+        boolean result = service.bookRoom(form, 2L, 2L);
+        assertTrue(result);
+
+        boolean failedResult = service.bookRoom(form, 2L, 2L);
+        assertFalse(failedResult);
+        assertEquals(1, form.getErrors().size());
+        assertTrue(form.getErrors().contains(ResourceBundle.getBundle("forms", new Locale("en")).getString("errors.RoomDatesOverlap")));
+    }
+}
--- tests.sql
+++ tests.sql
@@ -104,3 +104,30 @@ insert into dao_test(name, age) values ('tes3', 12);
 insert into dao_test(name, age) values ('test4', 13);
 
 insert into dao_test_insert(name, age) values ('testUpd', 110);
+
+insert into room_class(id) values (1), (2), (3), (4);
+
+insert into room_class_translation(class_id, name, language)
+values (1, 'Cheap', 'en'), (1, 'Дешевый', 'ru'),
+       (2, 'Normal', 'en'), (2, 'Обычный', 'ru'),
+       (3, 'Half lux', 'en'), (3, 'Полу люкс', 'ru'),
+       (4, 'Lux', 'en'), (4, 'Люкс', 'ru');
+
+insert into rooms (number, status, name, price, capacity, class)
+values (1, default, 'Room 1', 100, 2, 1), (2, default, 'Room 2', 110, 3, 1), (3, default, 'Room 3', 120, 3, 1), (4, default, 'Room 4', 150, 4, 1),
+       (5, default, 'Normal Room 1', 200, 4, 2),(6, default, 'Normal Room 2', 220, 4, 2), (7, default, 'Normal Room 3', 250, 4, 2), (8, default, 'Normal Room 4', 300, 6, 2),
+       (9, default, 'Half lux Room 1', 350, 4, 3),(10, default, 'Half lux Room 2', 370, 5, 3), (11, default, 'Half lux Room 3', 400, 4, 3), (12, default, 'Half lux Room 4', 500, 6, 3),
+       (13, default, 'lux Room 1', 700, 8, 4),(14, default, 'lux Room 2', 1000, 8, 4);
+
+insert into roles(name) values ('user'), ('manager');
+
+insert into users(login, email, password, first_name, last_name)
+values ('vadim', 'vadim@gmail.com', md5('123'), 'Vadim', 'Demb'),
+       ('test', 'test@gmail.com', md5('12345'), 'Test', 'Testovich');
+
+update users set balance = 100000 where id = 2;
+
+insert into users(login, email, password, role, first_name, last_name)
+values ('admin', 'admin@gmail.com', md5('333'), 2, 'Admin', 'Adminsky');
+
+insert into room_registry(user_id, room_id, check_in_date, check_out_date) values (1, 1, date(now()), date(now()) + interval '7 day');
