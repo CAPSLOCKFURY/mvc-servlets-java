82cbf3d5ddcc59d4e072fa6bb88186a74dfecad0
vadim.dembitskii@gmail.com <vadim.dembitskii@gmail.com>
2022-03-20 15:11:38 +0200

    added some tests

--- src/test/java/service/AdminRoomsServiceTest.java
+++ src/test/java/service/AdminRoomsServiceTest.java
@@ -1,6 +1,8 @@
 package service;
 
 import db.ConnectionPool;
+import forms.CloseRoomForm;
+import forms.ReportConfigurationForm;
 import models.Room;
 import models.base.ordering.OrderDirection;
 import models.base.ordering.Orderable;
@@ -51,6 +53,42 @@ public class AdminRoomsServiceTest {
         assertEquals(9, rooms.size());
         List<Room> rooms2 = service.findSuitableRoomsForRequest("en", Date.valueOf(today.plusDays(14).format(dateFormat)), Date.valueOf(todayPlus7.plusDays(36).format(dateFormat)), orderable, pageable);
         assertEquals(10, rooms2.size());
+    }
+
+    @Test
+    public void testFindDataForRoomRegistryReport(){
+        ReportConfigurationForm reportConfigurationForm = new ReportConfigurationForm();
+        boolean isValid = reportConfigurationForm.validate();
+        assertTrue(isValid);
+        reportConfigurationForm.setCheckInDate(null);
+        reportConfigurationForm.setCheckOutDate(null);
+        Pageable pageable = new Pageable(1, 10);
+        service.findDataForRoomRegistryReport(reportConfigurationForm, pageable);
+
+        ReportConfigurationForm reportConfigurationForm2 = new ReportConfigurationForm();
+        reportConfigurationForm2.setCheckInDate("2022-03-20");
+        reportConfigurationForm2.setCheckOutDate(null);
+        service.findDataForRoomRegistryReport(reportConfigurationForm2, pageable);
+
+        ReportConfigurationForm reportConfigurationForm3 = new ReportConfigurationForm();
+        reportConfigurationForm3.setCheckInDate("2022-03-20");
+        reportConfigurationForm3.setCheckOutDate("2022-03-30");
+        service.findDataForRoomRegistryReport(reportConfigurationForm3, pageable);
+    }
+
+    @Test
+    public void closeOpenRoomTest(){
+        LocalDateTime today = new java.sql.Date(System.currentTimeMillis()).toLocalDate().atStartOfDay();
+        LocalDateTime todayPlus7 = today.plusDays(7);
+        DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
+        Long roomId = 12L;
+        CloseRoomForm form = new CloseRoomForm();
+        form.setEndDate(todayPlus7.format(dateFormat));
+        boolean isValid = form.validate();
+        assertTrue(isValid);
+        service.closeRoom(roomId, form);
+        service.openRoom(roomId);
     }
 
 }
