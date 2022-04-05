1b24525d57de20283cae78180d47446fd469ad0b
vadim.dembitskii@gmail.com <vadim.dembitskii@gmail.com>
2022-04-05 22:03:31 +0300

    removed unnecessary todo

--- src/main/java/models/base/mappers/PreparedStatementMapper.java
+++ src/main/java/models/base/mappers/PreparedStatementMapper.java
@@ -3,6 +3,5 @@ package models.base.mappers;
 import java.sql.PreparedStatement;
 
 public interface PreparedStatementMapper {
-    //TODO put prepared statement here as parameter
     void mapToPreparedStatement(PreparedStatement stmt);
 }
